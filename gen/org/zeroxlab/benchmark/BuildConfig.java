40dd1d1be39b8e90ef3a7feedd01242767fb20d5
Earl Du <duzexu92@gmail.com>
2019-10-26 15:40:56 +0800

    initial commit
    Signed-off-by: Earl Du <duzexu92@gmail.com>

---
+++ gen/org/zeroxlab/benchmark/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.zeroxlab.benchmark;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ jni/Application.mk
@@ -0,0 +1,4 @@
+#APP_ABI := arm64-v8a
+#NDK_TOOLCHAIN :=  aarch64-linux-android-4.9
+APP_ABI := x86
+NDK_TOOLCHAIN :=  x86-4.9
